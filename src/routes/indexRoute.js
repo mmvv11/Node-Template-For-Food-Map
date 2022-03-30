8233fdee1ab6b27c1a996a603f0d0bf6c4c236d0
jiwoon <mmvv11@naver.com>
2022-03-30 15:38:09 +0900

    초기 코드 세팅

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -2,15 +2,7 @@ module.exports = function (app) {
   const index = require("../controllers/indexController");
   const jwtMiddleware = require("../../config/jwtMiddleware");
 
-  app.post("/signin", index.signIn); // 로그인
-
-  // 식당 CRUD
-  app.post("/restaurant", jwtMiddleware, index.insertRestaurant); // 식당 create
-  app.get("/restaurants", index.getRestaurants); // 식당 read
-  app.patch("/restaurant", jwtMiddleware, index.updateRestaurant); // 식당 update
-  app.delete(
-    "/restaurant/:restaurantIdx",
-    jwtMiddleware,
-    index.deleteRestaurant
-  ); // 식당 delete
+  // 라우터 정의
+  // app.HTTP메서드(uri, 컨트롤러 콜백함수)
+  // app.get("/dummy", index.example);
 };
